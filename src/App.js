a94398df04a5082f64866970a6a77f47a9b45750
eakyel <e.akyel@outlook.com>
2021-05-13 18:20:59 +0300

    Create navbar component structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact />
+      </Switch>
+    </Router>
   );
 }
 
