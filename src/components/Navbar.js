3681a5cc246e59ba17c37841669ff640f46b8eaa
eakyel <e.akyel@outlook.com>
2021-05-14 22:36:00 +0300

    Add sign-up button for the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    window.innerWidth <= 960 ? setButton(false) : setButton(true);
+  };
+
+  React.useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             LIFAND
           </Link>
 
@@ -53,6 +66,8 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
