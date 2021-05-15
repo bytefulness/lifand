92b69f5bbd20dd5503520b02d05219d0aba8b9d3
eakyel <e.akyel@outlook.com>
2021-05-15 22:00:00 +0300

    Create home page component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" exact />
+        <Route path="/" exact component={Home} />
       </Switch>
     </Router>
   );
