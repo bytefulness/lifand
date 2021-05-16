32706f4ae2936b8d3155760bc65a17e873d8536b
eakyel <e.akyel@outlook.com>
2021-05-16 23:41:11 +0300

    Create pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/signup" component={SignUp} />
       </Switch>
     </Router>
   );
