f4d4856ac67fb70c49c057bd781c8e0f0ea0132b
eakyel <e.akyel@outlook.com>
2021-05-16 23:42:03 +0300

    Change the assets folder in structure

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import { Button } from "./Button";
 import "../App.css";
 import "./HeroSection.css";
-import video from "../videos/video-2.mp4";
 
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src={video} autoPlay loop muted />
+      <video src="./videos/video-2.mp4" autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
