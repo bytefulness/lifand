7120ee16d7f308b47830208d42e2e78417acf985
eakyel <e.akyel@outlook.com>
2021-05-17 22:49:22 +0300

    Fix issue for unresolved images

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import CardItem from "./CardItem";
 import "./Cards.css";
-import Images from "../";
 
 function Cards() {
   return (
@@ -11,7 +10,7 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="./images/img-9.jpg"
+              src="./images/img-1.jpg"
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
               label="Adventure"
               path="/services"
@@ -26,19 +25,19 @@ function Cards() {
 
           <ul className="cards__items">
             <CardItem
-              src="./images/img-9.jpg"
+              src="./images/img-3.jpg"
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
               label="Adventure"
               path="/services"
             />
             <CardItem
-              src="./images/img-2.jpg"
+              src="./images/img-4.jpg"
               text="Travel through the Island of Bali in Provate Cruise"
               label="Luxury"
               path="/services"
             />
             <CardItem
-              src="./images/img-2.jpg"
+              src="./images/img-5.jpg"
               text="Travel through the Island of Bali in Provate Cruise"
               label="Luxury"
               path="/services"
